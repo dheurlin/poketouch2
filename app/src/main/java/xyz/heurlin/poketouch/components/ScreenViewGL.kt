041cb278897e2c753c19fae3bf167fa16008ba8f
dheurlin <daniel.heurlin@gmail.com>
2023-04-28 20:54:40 +0200

    use proper row length to render gl texture

--- app/src/main/java/xyz/heurlin/poketouch/components/ScreenViewGL.kt
+++ app/src/main/java/xyz/heurlin/poketouch/components/ScreenViewGL.kt
@@ -10,6 +10,12 @@ import java.nio.FloatBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
+private object GameBoyDimensions {
+    const val width = 160
+    const val height = 144
+    const val rowLength = 256
+    val rowLengthDiff get() = rowLength - width
+}
 
 class ScreenViewGL(private val context: Context) : GLSurfaceView(context), IScreenView {
     private val renderer = Renderer(context)
@@ -33,7 +39,7 @@ class ScreenViewGL(private val context: Context) : GLSurfaceView(context), IScre
         private val textures = intArrayOf(0)
         private val texId get() = textures[0]
 
-        private val data = ByteBuffer.allocateDirect(144 * 256 * 2)
+        private var data: ByteBuffer? = null
 
         private val vertex = makeFloatBuffer(
             -1.0f, +1.0f, 0.0f,
@@ -59,17 +65,13 @@ class ScreenViewGL(private val context: Context) : GLSurfaceView(context), IScre
 
             gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_NEAREST);
             gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
-            gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
-            gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
-
-//            gl.glPixelStorei(GL10.GL_UNPACK_ALIGNMENT, 4)
 
             gl.glTexImage2D(
                 GL10.GL_TEXTURE_2D,
                 0,
                 GL10.GL_RGB,
-                160,
-                144,
+                GameBoyDimensions.rowLength,
+                GameBoyDimensions.height,
                 0,
                 GL10.GL_RGB,
                 GL10.GL_UNSIGNED_SHORT_5_6_5,
@@ -79,14 +81,50 @@ class ScreenViewGL(private val context: Context) : GLSurfaceView(context), IScre
 
         override fun onSurfaceChanged(gl: GL10, width: Int, height: Int) {
             gl.glViewport(0, 0, width, height);
+            setTextureCordinates(width, height)
+        }
+
+        private fun setTextureCordinates(screenWidth: Int, screenHeight: Int) {
+            val nearestIntegerScaleFactor = screenWidth / GameBoyDimensions.width
+            val marginXInPixels = (screenWidth - ((GameBoyDimensions.rowLengthDiff) * nearestIntegerScaleFactor)) / 2
+            val marginYInPixels = (screenHeight - (GameBoyDimensions.height * nearestIntegerScaleFactor)) / 2
+
+            val minX = 0.0f - (marginXInPixels.toFloat() / screenWidth .toFloat())
+            val maxX = 1.0f + (marginXInPixels.toFloat() / screenWidth .toFloat())
+            val minY = 0.0f - (marginYInPixels.toFloat() / screenHeight.toFloat())
+            val maxY = 1.0f + (marginYInPixels.toFloat() / screenHeight.toFloat())
+
+            println("minXFraction, $minX")
+            println("minYFraction, $minY")
+            println("maxXFraction, $maxX")
+            println("maxYFraction, $maxY")
+
+            texcoords.apply {
+                put(0, minX); put(1, minY)
+                put(2, maxX); put(3, minY)
+                put(4, minX); put(5, maxY)
+                put(6, maxX); put(7, maxY)
+                rewind()
+            }
         }
 
         override fun onDrawFrame(gl: GL10) {
+            gl.glClear(GL10.GL_COLOR_BUFFER_BIT)
+
             gl.glActiveTexture(GL10.GL_TEXTURE0);
             gl.glBindTexture(GL10.GL_TEXTURE_2D, texId);
 
-            gl.glTexSubImage2D(GL10.GL_TEXTURE_2D, 0, 0, 0, 160, 144,
-                GL10.GL_RGB, GL10.GL_UNSIGNED_SHORT_5_6_5, data);
+            gl.glTexSubImage2D(
+                GL10.GL_TEXTURE_2D,
+                0,
+                0,
+                0,
+                GameBoyDimensions.rowLength,
+                GameBoyDimensions.height,
+                GL10.GL_RGB,
+                GL10.GL_UNSIGNED_SHORT_5_6_5,
+                data
+            );
 
             gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertex);
             gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, texcoords);
@@ -94,9 +132,7 @@ class ScreenViewGL(private val context: Context) : GLSurfaceView(context), IScre
         }
 
         fun videoRefresh(buffer: ByteBuffer, width: Int, height: Int, pitch: Long) {
-            data.rewind()
-            data.put(buffer)
-            data.rewind()
+            data = buffer
         }
 
     }
